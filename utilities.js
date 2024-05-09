f35f83657280dd8993270f9fa4949977c697cf69
Anjali <82653640+anjalikhushi@users.noreply.github.com>
2024-05-09 18:32:19 +0530

    Add files via upload

---
+++ utilities.js
@@ -0,0 +1,23 @@
+export const drawRect= (detection, ctx) =>{
+    detections.array.forEach(prediction=> {
+        
+        //Get prediction results
+        const [x,y,width,height] = prediction['bbox'];
+        const text = prediction['class'];
+   
+   //Set styling
+
+   const color='#' +Math.floor(Math.random()*16777215).toString(16);
+   ctx.strokeStyle=color
+   ctx.front='18px Arial'
+   ctx.fillStyle=color
+    //Draw rectangles and text
+    ctx.beginPath()
+    ctx.fillText(text,x,y)
+    ctx.rect(x,y,width,height)
+    ctx.stroke()
+   
+   
+    });
+
+}
